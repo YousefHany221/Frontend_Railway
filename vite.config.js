25afd87d593966f27972efa1242c28aee0f1233c
YousefHany221 <146834220+YousefHany221@users.noreply.github.com>
2026-06-19 19:15:23 +0300

    fix: set absolute base path in vite config to fix css on subroutes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 🎯 السطر السحري اللي هيصلح الـ CSS في الـ Login والـ Register
 })
